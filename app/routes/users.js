90fcca12a1d37f17109b6d251bf16fed8b5d462c
Seyed Arshia Ghaffarian <arshia.ghaffarian003@gmail.com>
2021-12-15 19:16:37 +0330

    Delete user(Routes)

--- app/routes/users.js
+++ app/routes/users.js
@@ -5,4 +5,5 @@ const usersController = require('../controllers/usersController')
 router.get('/', usersController.usersList) //!Route: /api/v1/users
 router.post('/', usersController.addUser) 
 router.get('/:id', usersController.getUser) 
+router.delete('/:id', usersController.removeUser)
 module.exports = router

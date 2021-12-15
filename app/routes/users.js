dc8f96b41c5f9e50be917eb90dec812950f91a5d
Seyed Arshia Ghaffarian <arshia.ghaffarian003@gmail.com>
2021-12-15 19:06:33 +0330

    User details(Routes)

--- app/routes/users.js
+++ app/routes/users.js
@@ -4,4 +4,5 @@ const usersController = require('../controllers/usersController')
 
 router.get('/', usersController.usersList) //!Route: /api/v1/users
 router.post('/', usersController.addUser) 
+router.get('/:id', usersController.getUser) 
 module.exports = router

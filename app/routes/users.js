9678d741fe831010ddb707a0cf71b80238adce51
Seyed Arshia Ghaffarian <arshia.ghaffarian003@gmail.com>
2021-12-15 22:39:52 +0330

    User update(Routes)

--- app/routes/users.js
+++ app/routes/users.js
@@ -6,4 +6,5 @@ router.get('/', usersController.usersList) //!Route: /api/v1/users
 router.post('/', usersController.addUser) 
 router.get('/:id', usersController.getUser) 
 router.delete('/:id', usersController.removeUser)
+router.patch('/:id', usersController.updateUser) 
 module.exports = router

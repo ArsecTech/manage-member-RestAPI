8f5842c92a87f2ba35cfba4aa06c69f1e923f23b
Seyed Arshia Ghaffarian <arshia.ghaffarian003@gmail.com>
2021-12-16 12:50:36 +0330

    Implementing Auth Middleware(routes)

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-unused-vars */
 const userRouter = require('./users')
+const auth = require('../middlewares/auth')
 module.exports = (app) => {
-    app.use('/api/v1/users', userRouter)
+  app.use('/api/v1/users', userRouter)
 }
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/usersController')
+const auth = require('../middlewares/auth')
 
-router.get('/', usersController.usersList) //!Route: /api/v1/users
+
+router.get('/', [auth], usersController.usersList) //!Route: /api/v1/users
 router.post('/', usersController.addUser) 
 router.get('/:id', usersController.getUser) 
 router.delete('/:id', usersController.removeUser)
